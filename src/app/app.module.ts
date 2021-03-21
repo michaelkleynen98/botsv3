3fed85fa86a0b78e5290d1c3a99d84c4ef60d42b
Michael Kleynen <michaelkleynen@gmail.com>
2021-03-21 20:03:41 +0100

    firebase update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,24 @@ import { ContactComponent } from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './products/products.component';
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { config } from 'rxjs';
+import { RegisterComponent } from './admin/register/register.component';
+import { ForgotPasswordComponent } from './admin/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './admin/verify-email/verify-email.component';
+
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDjkVoGFiyqDLjTNwCvc7bE7oayJImRRag",
+  authDomain: "dbotsfinal.firebaseapp.com",
+  projectId: "dbotsfinal",
+  storageBucket: "dbotsfinal.appspot.com",
+  messagingSenderId: "726212118380",
+  appId: "1:726212118380:web:fc705c4497dd40dde81549",
+  measurementId: "G-XTQC8T2ZGX"
+};
 
 const routes: Routes = [
   { path: '', redirectTo: 'main', pathMatch: 'full'},
@@ -16,8 +34,14 @@ const routes: Routes = [
   { path: 'products', component: ProductsComponent },
   { path:'contact', component: ContactComponent},
   { path:'about-us', component: AboutUsComponent},
-  { path:'login', component: LoginComponent}
+  { path:'login', component: LoginComponent},
+
+  //firebase
+  { path:  'register', component:  RegisterComponent },
+  { path:  'forgot-password', component:  ForgotPasswordComponent },
+  { path:  'verify-email', component:  VerifyEmailComponent }
   
+
 
 ];
 
@@ -28,9 +52,14 @@ const routes: Routes = [
     AboutUsComponent,
     ContactComponent,
     LoginComponent,
-    ProductsComponent
+    ProductsComponent,
+    RegisterComponent,
+    ForgotPasswordComponent,
+    VerifyEmailComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(config),
+    AngularFireAuthModule,
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(routes)
@@ -38,4 +67,6 @@ const routes: Routes = [
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+}
